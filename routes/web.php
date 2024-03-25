5ac9f836a75fce122434e21c4f5ae5ea590b27a5
Lamia Koucem <54423216+RiadhHS@users.noreply.github.com>
2024-03-25 09:45:35 +0100

    overwriting files

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-use App\Http\Controllers\CoproprietaireController;
-use App\Http\Controllers\DepotController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VisiteurController;
-use App\Http\Controllers\IndexController;
-use App\Http\Controllers\InscriptionAGController;
-use App\Http\Controllers\InscriptionOJAGController;
-Route::get('/', [IndexController::class, "index"])->name("/");
 
-Route::get('/visiteurs', [VisiteurController::class, 'index'])->name("visiteurs");
-Route::get('/coproprietaire', [CoproprietaireController::class, 'index'])->name("coproprietaire"); 
-Route::get('/inscriptionag', [InscriptionAGController::class, 'index'])->name("inscriptionag"); 
-Route::get('/inscriptionojag', [InscriptionOJAGController::class, 'index'])->name("inscriptionojag"); 
-Route::get('/depot', [DepotController::class, 'index'])->name("depot");
+Route::get('/', function () {
+    return view('welcome');
+});
